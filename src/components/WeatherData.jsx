834b42713ad2e0cefec9185d4ac3529c58d48343
Aayush-Koju <iush.cozu12@gmail.com>
2024-08-01 12:19:40 +0545

    added icon for weather conditions

--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -1,25 +1,35 @@
 import React from "react";
+// import { useState } from "react";
 
 export default function WeatherData({ data }) {
+  // const [image, setImage] = useState("");
+
   if (!data || !data.main) {
     return <div>No data available. Please enter a city name and search.</div>;
   }
 
   const weather = data.weather && data.weather[0];
 
+  const image = ` https://openweathermap.org/img/wn/${weather.icon}@2x.png`;
+
   return (
-    <div className="weather-data">
-      <h2>{data.name}</h2>
-      <p>
-        Temperature: {data.main.temp ? `${data.main.temp} °C` : "Not available"}
-      </p>
-      {weather && (
-        <>
-          <p>Weather: {weather.main}</p>
-          <p>Description: {weather.description}</p>
-        </>
-      )}
-      <p>Humidity: {data.main.humidity}%</p>
-    </div>
+    <>
+      <img src={image} alt={weather.description} />
+      <div className="weather-data">
+        <h2>{data.name}</h2>
+        <p>
+          Temperature:{" "}
+          {data.main.temp ? `${data.main.temp} °C` : "Not available"}
+        </p>
+        {weather && (
+          <>
+            <p>Weather: {weather.main}</p>
+            <p>Description: {weather.description}</p>
+            {/* <p>Description: {weather.icon}</p> */}
+          </>
+        )}
+        <p>Humidity: {data.main.humidity}%</p>
+      </div>
+    </>
   );
 }
