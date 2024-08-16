b15b553e32b25b946d1c56d739cd2c5b73067eaa
Aayush-Koju <iush.cozu12@gmail.com>
2024-08-16 13:04:58 +0545

    added background color to app info component

--- src/components/AppInfo.jsx
+++ src/components/AppInfo.jsx
@@ -3,8 +3,10 @@ import "./AppInfo.css";
 export default function AppInfo() {
   return (
     <>
-      <h1>Weather App</h1>
-      <p>Get the weather data for any location.</p>
+      <div className="app-info">
+        <h1>Weather App</h1>
+        <p>Get the weather data for any location.</p>
+      </div>
     </>
   );
 }
